e8c7b4e55c5ef58b528d39ace9889565ebc3aaf8
Dmytro Medvediev <dsisitkin@gmail.com>
2023-07-13 23:13:31 +0200

    Update ContactForm.styled.js

--- src/components/ContactForm/ContactForm.styled.js
+++ src/components/ContactForm/ContactForm.styled.js
@@ -3,6 +3,7 @@ import styled from 'styled-components';
 export const Form = styled.form`
   display: flex;
   flex-direction: column;
+  width: 100%;
   max-width: 300px;
   margin-bottom: 20px;
 `;
@@ -15,9 +16,11 @@ export const Label = styled.label`
 export const Input = styled.input`
   padding: 10px;
   margin-top: 5px;
+  margin-bottom: 10px;
   border: none;
   border-radius: 5px;
   background-color: #f4f4f4;
+  width: 100%;
 `;
 
 export const Button = styled.button`
