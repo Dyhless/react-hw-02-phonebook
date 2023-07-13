dcf5535dc1761ecf97b25521df735a612a3264dc
Dmytro Medvediev <dsisitkin@gmail.com>
2023-07-13 22:37:00 +0200

    Update Filter.styled.js

---
+++ src/components/Filter/Filter.styled.js
@@ -0,0 +1,10 @@
+import styled from 'styled-components';
+
+export const Wrapper = styled.label`
+  margin-bottom: 10px;
+`;
+
+export const Input = styled.input`
+  padding: 5px;
+  margin-top: 5px;
+`;
