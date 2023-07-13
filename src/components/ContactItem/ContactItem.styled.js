11a4308d5d2d21aa3eff260e286c7088f93702b1
Dmytro Medvediev <dsisitkin@gmail.com>
2023-07-13 22:36:43 +0200

    Update ContactItem.styled.js

---
+++ src/components/ContactItem/ContactItem.styled.js
@@ -0,0 +1,28 @@
+import styled from 'styled-components';
+
+export const Item = styled.li`
+  margin-bottom: 10px;
+`;
+
+export const Info = styled.div`
+  display: flex;
+  align-items: center;
+`;
+
+export const Name = styled.p`
+  font-weight: bold;
+  margin-right: 10px;
+`;
+
+export const Number = styled.p`
+  color: gray;
+`;
+
+export const DeleteButton = styled.button`
+  padding: 2px 5px;
+  background-color: #ff3d00;
+  color: #fff;
+  border: none;
+  cursor: pointer;
+  font-size: 14px;
+`;
