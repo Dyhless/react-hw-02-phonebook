245a079ebd7d15f94e04c367ebbe0671cbd44885
Dmytro Medvediev <dsisitkin@gmail.com>
2023-07-13 23:08:07 +0200

    Update Filter.styled.js

--- src/components/Filter/Filter.styled.js
+++ src/components/Filter/Filter.styled.js
@@ -2,9 +2,13 @@ import styled from 'styled-components';
 
 export const Wrapper = styled.label`
   margin-bottom: 10px;
+  font-weight: bold;
 `;
 
 export const Input = styled.input`
-  padding: 5px;
+  padding: 10px;
   margin-top: 5px;
+  border: none;
+  border-radius: 5px;
+  background-color: #f4f4f4;
 `;
